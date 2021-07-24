4385ab756af791bcaf8b097cc986946957a40956
Alex Butnaru <alex.butnaru1102@gmail.com>
2021-07-24 21:08:01 +0300

    Created and styled shop page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import Homepage from "./pages/Homepage";
 import "./styles/app.scss";
 import { Switch, Route } from "react-router-dom";
+import Shop from "./pages/Shop";
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={Homepage} />
+        <Route exact path='/shop' component={Shop} />
       </Switch>
     </div>
   );
