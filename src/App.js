b8efac3ab1115697fd3960d4e4fc1786dbbc9cf9
Alex Butnaru <alex.butnaru1102@gmail.com>
2021-07-24 20:00:41 +0300

    Created and styled initial directory and menu-item components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
+import Homepage from "./pages/Homepage";
+import "./styles/app.scss";
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div>
+      <Homepage />
+    </div>
+  );
 }
 
 export default App;
